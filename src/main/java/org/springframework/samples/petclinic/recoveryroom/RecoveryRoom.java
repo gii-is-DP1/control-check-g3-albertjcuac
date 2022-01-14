cadb7b649fcf9669a70963808befd07ab1fc34e0
albmonmar3 <albmonmar3@alum.us.es>
2022-01-14 16:37:06 +0100

    entrega

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoom.java
@@ -1,17 +1,32 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 
 import lombok.Getter;
 import lombok.Setter;
-
+@Entity
 @Getter
 @Setter
-public class RecoveryRoom {
-    Integer id;
-    String name;
-    double size;
-    boolean secure;
-    @Transient
-    RecoveryRoomType roomType;
+public class RecoveryRoom  extends BaseEntity {
+	  @NotNull	
+	  @Size(min = 3, max = 50)
+	  @Column(name = "name")
+	  private String name;
+	  @NotNull
+	  @Min(0)
+	  private double size;
+	  @NotNull
+	  private  boolean secure;
+	  
+	  @ManyToOne(optional=false)
+	  RecoveryRoomType roomType;
 }
--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomTypeFormatter.java
@@ -3,20 +3,33 @@ package org.springframework.samples.petclinic.recoveryroom;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RecoveryRoomTypeFormatter implements Formatter<RecoveryRoomType>{
 
+	
+	private final RecoveryRoomService roomService;
+
+	@Autowired
+	public RecoveryRoomTypeFormatter(RecoveryRoomService roomService) {
+		this.roomService = roomService;
+	}
     @Override
     public String print(RecoveryRoomType object, Locale locale) {
-        return null;
+        return  object.getName();
     }
 
     @Override
     public RecoveryRoomType parse(String text, Locale locale) throws ParseException {
-        return null;
-    }
-    
+    	RecoveryRoomType roomType = this.roomService.getRecoveryRoomType(text);
+    	
+    			if (roomType!=null) {
+    				return roomType;
+    			
+    		}
+    		throw new ParseException("type not found: " + text, 0);
+        }
 }
